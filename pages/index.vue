ccb29210ddbaef4d75a2d4c86ccd1f9a801cbec4
ww2or3ww <w2or3w@gmail.com>
2020-12-06 13:28:56 +0000

    for dev

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
       </div>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          Welcome to the Vuetify + Nuxt.js template (開発環境です！)
         </v-card-title>
         <v-card-text>
           <p>
